8baf2e2168f02bccb5a51a59d2890187bf2b5664
Dhirajchaudari <dhirajchaudhari789@gmail.com>
2023-06-13 16:34:26 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>TS Amazone</header>
+      <main></main>
+      <footer>All Rights ar Reserved</footer>
+    </div>
+  );
+}
+
+export default App;
